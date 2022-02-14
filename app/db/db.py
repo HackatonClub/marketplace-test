cb571c4ffe098994ebb497c11fdc3f939a05a14a
huscker <egostalker03@mail.ru>
2022-02-14 21:16:32 +0300

    Исправил баги предыдущего комита & Добавил следующие функции взаимодействия с бд: удаление тегов от продукта, удаление тегов, добавление покупателя, удаление избранного, добавлние избранного

--- app/db/db.py
+++ app/db/db.py
@@ -4,6 +4,7 @@ import asyncpg
 from app.settings import DATABASE_URL,DB_DATABASE,DB_HOST,DB_USER,DB_PASSWORD,DB_PORT
 
 # TODO: add_tag_to_product_by_id возможно можно быстрее сделать
+# TODO: add_favourite в один запрос сделать
 
 class DB:
     con : asyncpg.connection.Connection = None
@@ -19,15 +20,17 @@ class DB:
             print(er)
             cls.con = None
             return False
+        return True
 
     @classmethod
     async def add_new_tag(cls,name: str):
         sql = "insert into tags (name) values ($1) ;"
         try:
-            return await cls.con.execute(sql, name )
+            await cls.con.execute(sql, name )
         except Exception as e:
             print(e)
             return False
+        return True
 
     @classmethod
     async def add_tag_to_product_by_id(cls,tag_name:str,product_id:int):
@@ -55,6 +58,39 @@ class DB:
         return True
 
     @classmethod
+    async def remove_tag_from_product_by_id(cls,name:str,product_id:int):
+        sql = 'delete from tags_product where product_id = $1 and tag_id in (select id from tags where name = $2)'
+        try:
+            await cls.con.execute(sql, product_id,name)
+        except Exception as er:
+            print(er)
+            return False
+        return True
+
+    @classmethod
+    async def remove_tag(cls,name:str):
+        id = None
+        sql = 'select id from tags where name = $1'
+        try:
+            id = await cls.con.fetchrow(sql,name)
+        except Exception as er:
+            print(er)
+            return False
+        sql = 'delete from tags_product where tag_id = $1'
+        try:
+            await cls.con.execute(sql,id)
+        except Exception as er:
+            print(er)
+            return False
+        sql = 'delete from tags where id = $1'
+        try:
+            await cls.con.execute(sql, id)
+        except Exception as er:
+            print(er)
+            return False
+        return True
+
+    @classmethod
     async def get_all_tags(cls):
         sql = "select name from tags"
         try:
@@ -72,3 +108,43 @@ class DB:
             print(e)
             return False
 
+    @classmethod
+    async def add_customer(cls,name:str):
+        sql = "insert into customer(name) values ($1)"
+        try:
+            await cls.con.execute(sql,name)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    @classmethod
+    async def add_favourite(cls,name:str,product_id:int):
+        id = None
+        sql = "select id from customer where name = $1"
+        try:
+            id = await cls.con.fetchrow(sql,name)
+        except Exception as e:
+            print(e)
+            return False
+        sql = "insert into favourite(customer_id, product_id) VALUES ($1,$2)"
+        try:
+            await cls.con.execute(sql, id,product_id)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    @classmethod
+    async def remove_favourite(cls,name:str,product_id:int):
+        sql = 'delete from favourite where customer_id in (select id from customer where name = $1)'
+        try:
+            await cls.con.execute(sql)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    @classmethod
+    async def remove_customer(cls,name:str):
+        pass
