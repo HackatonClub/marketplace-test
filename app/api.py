1c2ddca7df46265fb01bf12a2c0edc85178428f6
huscker <egostalker03@mail.ru>
2022-02-15 11:55:39 +0300

    Подключил рутеры к fast api

--- app/api.py
+++ app/api.py
@@ -1,5 +1,25 @@
+import sys
+
 from fastapi import FastAPI
+from app.tags.api import tags_router
+from app.customer.api import customer_router
+from app.db.db import DB
+
+app = FastAPI(
+    title='Marketplace'
+)
 
 
+@app.on_event('startup')
+async def startup():
+    if not await DB.connect_db():
+        sys.exit(0)
 
-app = FastAPI()
+
+@app.on_event('shutdown')
+async def shutdown():
+    await DB.disconnect_db()
+
+
+app.include_router(tags_router)
+app.include_router(customer_router)
--- app/tags/api.py
+++ app/tags/api.py
@@ -2,12 +2,15 @@ from typing import Optional
 from fastapi import APIRouter, status, HTTPException, Header
 from fastapi.responses import JSONResponse
 
+from app.formatter import format_records
+
 from app.db.db import DB
 
 tags_router = APIRouter()
 
+
 @tags_router.post('/tag')
-async def add_tag(tag_name : Optional[str] = Header(None,description='Имя тега')):
+async def add_tag(tag_name: Optional[str] = Header(None, description='Имя тега')):
     if not await DB.add_new_tag(tag_name):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -23,18 +26,19 @@ async def product_add_tag(product_id: Optional[int] = Header(None,description='I
     if not await DB.add_tag_to_product_by_id( tag_name,product_id):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Тэг уже присвоен'
+            detail='Нет заданного продукта или тэг уже присвоен'
         )
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={
         'details': 'Executed'
     })
 
-@tags_router.post('/customer')
-async def add_customer(name : Optional[str] = Header(None,description='Имя покупателя')):
-    if not await DB.add_customer(name):
+
+@tags_router.delete('/tag')
+async def delete_tag(tag_name: Optional[str] = Header(None, description='Имя тэга')):
+    if not await DB.remove_tag(tag_name):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Покупатель с таким именем существует'
+            detail='Тэг уже удален'
         )
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={
         'details': 'Executed'
@@ -52,27 +56,10 @@ async def remove_tag_from_product(product_id: Optional[int] = Header(None,descri
         'details': 'Executed'
     })
 
-@tags_router.delete('/favourite')
-async def delete_favourite(product_id: Optional[int] = Header(None,description='Id продукта'),
-                          tag_name: Optional[str] = Header(None,description='Имя покупателя')):
-    if not await DB.remove_favourite( tag_name,product_id):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Уже удален из избранного'
-        )
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content={
-        'details': 'Executed'
-    })
-
-@tags_router.delete('/customer')
-async def delete_customer(name: Optional[str] = Header(None,description='Имя покупателя')):
-    pass
-
 @tags_router.get('/tag')
 async def get_all_tags():
     tags = await DB.get_all_tags()
-    if not tags:
-        tags = []
+    tags = format_records(tags)
     return JSONResponse(status_code=status.HTTP_200_OK,content={
         'tags':tags
     })
@@ -80,11 +67,7 @@ async def get_all_tags():
 @tags_router.get('/product/tag')
 async def get_tags_of_product(product_id: Optional[int] = Header(None,description='Id продукта')):
     tags = await DB.get_tags_of_product_by_id(product_id)
-    if not tags:
-        tags = []
+    tags = format_records(tags)
     return JSONResponse(status_code=status.HTTP_200_OK,content={
         'tags':tags
     })
-@tags_router.get('/customer')
-async def get_customers():
-    pass
