251efc7819a61b18be05a7b35b623cfdd8aab631
huscker <egostalker03@mail.ru>
2022-02-22 21:21:14 +0300

    Добавил поиск по тэгам

--- app/queries/tag.py
+++ app/queries/tag.py
@@ -1,5 +1,5 @@
 from app.db.db import DB
-
+from app.utils.formatter import get_col_values
 
 async def add_new_tag(name: str):
     sql = "insert into tags (name) values ($1) ;"
@@ -23,7 +23,6 @@ async def remove_tag_from_product_by_id(tag_name: str, product_id: int):
 
 
 async def remove_tag(tag_name: str):
-    customer_id = None
     sql = 'select id from tags where name = $1'
     customer_id = (await DB.fetchrow(sql, tag_name))['id']
     sql = 'delete from tags_product where tag_id = $1'
@@ -40,3 +39,12 @@ async def get_all_tags():
 async def get_tags_of_product_by_id(id: int):
     sql = "select tags.name from tags_product join tags on tags.id = tags_product.tag_id where tags_product.product_id = $1;"
     return await DB.fetch(sql, id)
+
+
+async def get_products_by_tags(tags: list):
+    if not tags:
+        return False
+    sql = "select id from tags where name = ANY($1::text[])"
+    tag_ids = get_col_values(await DB.fetch(sql, tags), 'id')
+    sql = "with ids as (select product_id,count(tag_id) from tags_product as t where t.tag_id = ANY($1::int[]) group by product_id) select p.id from product as p join ids on p.id = ids.product_id where ids.count = $2;"
+    return await DB.fetch(sql, tag_ids, len(tags))
