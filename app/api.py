53d0e19128ca5e07067f6e14af9ae26dc6dfed37
Nicialy <andrey24072002@bk.ru>
2022-02-23 01:46:59 +0300

    Исправил ошибки & начал работу над продуктами3

--- app/api.py
+++ app/api.py
@@ -7,14 +7,11 @@ from app.routers.cart import cart_router
 from app.routers.customer import customer_router
 from app.routers.favourite import favourite_router
 from app.routers.photo import photo_router
-<<<<<<< HEAD
 from app.routers.product import product_router
 from app.db.db import DB
-=======
 from app.routers.registr import registr_router
 from app.routers.review import review_router
 from app.routers.tag import tags_router
->>>>>>> e503a361e171617a87de016e1e19d7c0f070356d
 
 app = FastAPI(
     title='Marketplace'
@@ -38,8 +35,5 @@ app.include_router(cart_router)
 app.include_router(registr_router)
 app.include_router(favourite_router)
 app.include_router(photo_router)
-<<<<<<< HEAD
 app.include_router(product_router)
-=======
 app.include_router(review_router)
->>>>>>> e503a361e171617a87de016e1e19d7c0f070356d
