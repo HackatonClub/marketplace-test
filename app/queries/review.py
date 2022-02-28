6095be833a1215475f81da185bfe1a54d0ab4965
huscker <egostalker03@mail.ru>
2022-02-28 16:45:13 +0300

    closed #10

--- app/queries/review.py
+++ app/queries/review.py
@@ -11,12 +11,7 @@ async def add_review_to_product(customer_name: str, product_id: int, body: str, 
     customer_id = customer_id['id']
     sql = "insert into review(product_id, customer_id, body, rating) VALUES ($1,$2,$3,$4)"
     await DB.execute(sql, product_id, customer_id, body, rating)
-    sql = "select sum(rating),count(customer_id) from review where product_id = $1"
-    temp = await DB.fetchrow(sql, product_id)
-    if temp['sum'] is None:
-        temp = {'sum': 0, 'count': 1}
-    sql = "update product set num_reviews = $2, avg_rating = $1 where id = $3"
-    return await DB.execute(sql, temp['sum'] / temp['count'], temp['count'], product_id)
+    return update_product_dynamic_data(product_id)
 
 
 async def delete_review_from_product(customer_name: str, product_id: int):
@@ -27,12 +22,7 @@ async def delete_review_from_product(customer_name: str, product_id: int):
     customer_id = customer_id['id']
     sql = "delete from review where customer_id = $1 and product_id = $2"
     await DB.execute(sql, customer_id, product_id)
-    sql = "select sum(rating),count(customer_id) from review where product_id = $1"
-    temp = await DB.fetchrow(sql, product_id)
-    if temp['sum'] is None:
-        temp = {'sum': 0, 'count': 1}
-    sql = "update product set num_reviews = $2, avg_rating = $1 where id = $3"
-    return await DB.execute(sql, temp['sum'] / temp['count'], temp['count'], product_id)
+    return update_product_dynamic_data(product_id)
 
 
 async def update_review_to_product(customer_name: str, product_id: int, body: str, rating: int):
@@ -43,6 +33,10 @@ async def update_review_to_product(customer_name: str, product_id: int, body: st
     customer_id = customer_id['id']
     sql = "update review set body = $1, rating = $2 where customer_id = $3 and product_id = $4"
     await DB.execute(sql, body, rating, customer_id, product_id)
+    return await update_product_dynamic_data(product_id)
+
+
+async def update_product_dynamic_data(product_id: int):
     sql = "select sum(rating),count(customer_id) from review where product_id = $1"
     temp = await DB.fetchrow(sql, product_id)
     if temp['sum'] is None:
