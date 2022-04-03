413a1b372aab8cc659ca6ebcaee42def1bcd5549
huscker <egostalker03@mail.ru>
2022-04-04 00:02:58 +0300

    Добавил роли юзерам

--- app/db/initial.sql
+++ app/db/initial.sql
@@ -1,14 +1,15 @@
-create table customer
+create table if not exists users
 (
     id       serial
         constraint customer_pk
             primary key,
     name     varchar(256) not null unique,
-    password text         not null
+    password text         not null,
+    role integer not null
 );
 
 
-create table product
+create table if not exists product
 (
     id          serial
         constraint product_pk
@@ -22,7 +23,7 @@ create table product
     tag_id jsonb
 );
 
-create table tags
+create table if not exists tags
 (
     id   serial
         constraint tags_pk
@@ -31,41 +32,41 @@ create table tags
         unique
 );
 
-create table tags_product
+create table if not exists tags_product
 (
-    tag_id integer references tags (id),
-    product_id integer references product (id),
+    tag_id integer references tags (id) on delete cascade,
+    product_id integer references product (id) on delete cascade,
     tag_ids jsonb
 );
 
-create table cart_product
+create table if not exists cart_product
 (
     id          serial
         constraint cart_product_pk
             primary key,
-    customer_id integer references customer (id) on delete cascade,
+    customer_id integer references users (id) on delete cascade,
     product_id  integer references product (id) on delete cascade,
     product_num integer not null,
     unique (customer_id, product_id)
 );
 
-create table favourite
+create table if not exists favourite
 (
     id          serial
         constraint favourite_pk
             primary key,
-    customer_id integer references customer (id) on delete cascade,
+    customer_id integer references users (id) on delete cascade,
     product_id  integer references product (id) on delete cascade,
     unique (customer_id, product_id)
 );
 
-create table review
+create table if not exists review
 (
     id          serial
         constraint review_pk
             primary key,
     product_id  integer references product (id) on delete cascade,
-    customer_id integer references customer (id) on delete cascade,
+    customer_id integer references users (id) on delete cascade,
     body        varchar(256),
     rating      integer,
     unique (product_id, customer_id)
